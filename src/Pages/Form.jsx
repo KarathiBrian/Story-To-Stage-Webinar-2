26a5fa4e19e2685cfa65ab5761a04e2336369ff5
KarathiBrian <thimanz58@gmail.com>
2025-07-29 16:28:54 +0300

    responder link

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import "../Css/Form.css";
 import Footer from '../Components/Footer';
-import formImage from "../assets/fomr img4.jpg"
+// import formImage from "../assets/fomr img4.jpg"
 const Form = () => {
   const [loading, setLoading] = useState(true);
 
@@ -27,7 +27,8 @@ const Form = () => {
 
 {/* <img src={formImage}  className='imgForm'/> */}
       <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdYvbLSVlpEurtSFptgzO4xfTmWHUkqAUGJbEhoyh_1zXvpXw/viewform?embedded=true"
+      src='https://docs.google.com/forms/d/e/1FAIpQLSdYvbLSVlpEurtSFptgzO4xfTmWHUkqAUGJbEhoyh_1zXvpXw/viewform?usp=sharing&ouid=100944596123287161908'
+        // src="https://docs.google.com/forms/d/e/1FAIpQLSdYvbLSVlpEurtSFptgzO4xfTmWHUkqAUGJbEhoyh_1zXvpXw/viewform?embedded=true"
         width="100%"
         height="1800"
         style={{ border: "none" }}
